a5730e7376f5a7169e6d441bf89bbc07a67e0b9a
Luis Hlatki <luis.hlatki@hotmail.com>
2022-09-28 10:56:04 -0300

    commit on master

--- dist/livechat/12.chunk.b9c74.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{2016:function(e){e.exports=JSON.parse('{"translation":{"are_you_sure_you_want_to_switch_the_department":"Jeste li sigurni da želite prebaciti odjel?"}}')}}]);
-//# sourceMappingURL=12.chunk.b9c74.js.map
--- dist/livechat/18.chunk.3bdaf.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{2022:function(n){n.exports=JSON.parse('{"translation":{}}')}}]);
-//# sourceMappingURL=18.chunk.3bdaf.js.map
